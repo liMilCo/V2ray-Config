fb0dbb48c24501a37ef5337c4702a314b58c1124
bardiafa <143993892+barry-far@users.noreply.github.com>
2025-07-09 10:51:25 -0400

    Added Mahsanet

--- Files/app.py
+++ Files/app.py
@@ -89,8 +89,14 @@ def main():
         "https://raw.githubusercontent.com/mfuu/v2ray/master/v2ray",
         "https://raw.githubusercontent.com/ts-sf/fly/main/v2",
         "https://raw.githubusercontent.com/aiboboxx/v2rayfree/main/v2",
-        "https://mrpooya.top/SuperApi/BE.php",
-        "https://raw.githubusercontent.com/MrPooyaX/VpnsFucking/main/BeVpn.txt"
+        "https://raw.githubusercontent.com/mahsanet/MahsaFreeConfig/refs/heads/main/mci/sub_1.txt",
+        "https://raw.githubusercontent.com/mahsanet/MahsaFreeConfig/refs/heads/main/mci/sub_2.txt",
+        "https://raw.githubusercontent.com/mahsanet/MahsaFreeConfig/refs/heads/main/mci/sub_3.txt",
+        "https://raw.githubusercontent.com/mahsanet/MahsaFreeConfig/refs/heads/main/app/sub.txt",
+        "https://raw.githubusercontent.com/mahsanet/MahsaFreeConfig/refs/heads/main/mtn/sub_1.txt",
+        "https://raw.githubusercontent.com/mahsanet/MahsaFreeConfig/refs/heads/main/mtn/sub_2.txt",
+        "https://raw.githubusercontent.com/mahsanet/MahsaFreeConfig/refs/heads/main/mtn/sub_3.txt",
+        "https://raw.githubusercontent.com/mahsanet/MahsaFreeConfig/refs/heads/main/mtn/sub_4.txt",
     ]
     dir_links = [
         "https://raw.githubusercontent.com/IranianCypherpunks/sub/main/config",
